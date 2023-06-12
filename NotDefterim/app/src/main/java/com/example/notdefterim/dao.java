06de0d3c19ca122edc2b1c265bb4a9baebfa5f8b
Ensaryrd <ensaryardmci@gmail.com>
2023-06-12 20:50:25 +0300

    Not Defteri Uygulaması
    Mobil Programlama Proje

---
+++ NotDefterim/app/src/main/java/com/example/notdefterim/dao.java
@@ -0,0 +1,26 @@
+package com.example.notdefterim;
+
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import java.util.List;
+
+@SuppressWarnings("AndroidUnresolvedRoomSqlReference")
+@Dao
+public interface dao {
+
+    @Insert
+    public void notEkle(Not not);
+
+    @Query("select * from notlar")
+    public List<Not> getNot();
+
+    @Delete
+    public void notSil(Not not);
+
+    @Update
+    public void notGuncelle(Not not);
+}
